85cb7578f2f07d70c1f178a7655fbf3ef4ff3a0c
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-12-19 16:11:12 +0100

    Tests : app.component

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,13 +1,24 @@
 import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './features/auth/services/auth.service';
+import { SessionService } from './services/session.service';
+import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let authService: AuthService;
+  let sessionService: SessionService;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -18,12 +29,71 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        AuthService, 
+        SessionService
+      ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    sessionService = TestBed.inject(SessionService);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should call $isLogged() and return true', () => {
+    // Arrange
+    const isLoggedSpy = jest.spyOn(sessionService, '$isLogged').mockReturnValue(of(true));
+
+    // Act
+    const result: Observable<boolean> = app.$isLogged();
+
+    // Assert
+    result.subscribe((value) => {
+      expect(value).toBe(true);
+      expect(isLoggedSpy).toHaveBeenCalled();
+    });
+  });
+
+  it('should call $isLogged() and return false', () => {
+    // Arrange
+    const isLoggedSpy = jest.spyOn(sessionService, '$isLogged').mockReturnValue(of(false));
+
+    // Act
+    const result: Observable<boolean> = app.$isLogged();
+
+    // Assert
+    result.subscribe((value) => {
+      expect(value).toBe(false);
+      expect(isLoggedSpy).toHaveBeenCalled();
+    });
+  });
+
+  it('should call logout() and navigate to home', () => {
+    // Arrange
+    const logOutSpy = jest.spyOn(sessionService, 'logOut');
+    const navigateSpy = jest.spyOn(router, 'navigate');
+
+    // Act
+    app.logout();
+
+    // Assert
+    expect(logOutSpy).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
+  });  it('should call logout() and navigate to home', () => {
+    // Arrange
+    const logOutSpy = jest.spyOn(sessionService, 'logOut');
+    const navigateSpy = jest.spyOn(router, 'navigate');
+
+    // Act
+    app.logout();
+
+    // Assert
+    expect(logOutSpy).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
   });
 });
