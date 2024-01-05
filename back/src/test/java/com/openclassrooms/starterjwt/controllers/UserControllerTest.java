9064c991b0adc9a9d9980d30e61703626fa8a791
Sandra VOISIN <sandra.voisin92@gmail.com>
2024-01-05 21:28:19 +0100

    Session service

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
@@ -0,0 +1,8 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
+public class UserControllerTest {
+
+}
